976ffbe471d9a78ad01f5d01b1451d6f8e0bcba3
atsutama2 <atsutama.0713@gmail.com>
2021-09-13 17:37:29 +0900

    Project追加

---
+++ Assets/Scripts/ButtonView.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ButtonView : MonoBehaviour
+{
+    public void ShowNativeDialog() {
+        #if UNITY_ANDROID
+        AndroidJavaClass nativeDialog = new AndroidJavaClass ("jp.co.test.mylibrary.AndroidNativeDialog");
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject context  = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+        context.Call ("runOnUiThread", new AndroidJavaRunnable(() => {
+            nativeDialog.CallStatic (
+                "showNativeDialog",
+                context,
+                "タイトル",
+                "本文テキスト"
+            );
+        }));
+        #elif UNITY_EDITOR
+            UnityEngine.Debug.Log("On Click!");
+        #endif
+    }
+}
