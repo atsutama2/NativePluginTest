3ce5b88051812e84b30f9fd6ff69f29a199f7d8e
atsutama2 <atsutama.0713@gmail.com>
2021-11-19 12:16:06 +0900

    Menuシーンからそれぞれ遷移出来るように修正

---
+++ Assets/Scripts/AppDefine.cs
@@ -0,0 +1,11 @@
+﻿public static class AppDefine
+{
+    // シーンリスト
+    public enum SceneType
+    {
+        None                       = 0,
+        MenuScene                  = 1,
+        AlertTestScene             = 2,
+        SpeechRecognitionTestScene = 3
+    }
+}
---
+++ Assets/Scripts/TransitionSceneButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionSceneButton : MonoBehaviour
+{
+    [SerializeField] private AppDefine.SceneType sceneName;
+
+    public void OnTransitionSceneStart()
+    {
+        if (string.IsNullOrEmpty(sceneName.ToString())) {
+            return;
+        }
+
+        SceneManager.LoadScene(sceneName.ToString());
+    }
+}
